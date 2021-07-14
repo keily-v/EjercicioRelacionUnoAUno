3ad4a940ca10155abffcc9a3624fd127d50503a6
Keily Vallejo <kmvallejo2@espe.edu.ec>
2021-07-14 10:54:13 -0500

    v0

---
+++ src/main/java/dominio/Empleado.java
@@ -0,0 +1,79 @@
+
+package dominio;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Empleado {
+    private String nombre;
+    private Fecha fechaN;
+    private Fecha fechaI;
+    
+    public Empleado(String nombre, Fecha fechaN, Fecha fechaI) {
+        this.nombre = nombre;
+        this.fechaN = fechaN;
+        this.fechaI = fechaI;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Fecha getFechaN() {
+        return fechaN;
+    }
+
+    public Fecha getFechaI() {
+        return fechaI;
+    }
+    
+   
+    public void calculoEdad (Fecha nFecha){
+       
+        Calendar fecha = new GregorianCalendar();
+        
+        int anioactual = fecha.get(Calendar.YEAR);
+        int mesactual = fecha.get(Calendar.MONTH);
+        int diaactual = fecha.get(Calendar.DAY_OF_MONTH);
+        
+        int aniocalculado = anioactual - nFecha.getAnio();
+        int mescalculado = mesactual - nFecha.getMes();
+        int diacalculado = diaactual - nFecha.getDia();
+        
+        System.out.println("Su edad es: "+aniocalculado+" años "+mescalculado+" meses "+diacalculado+" días");
+//        fechaN.setAnio(aniocalculado);
+//        fechaN.setMes(mescalculado);
+//        fechaN.setDia(diacalculado);
+       
+    } 
+    public void calculoTiempo (Fecha iFecha){
+        Calendar fecha= new GregorianCalendar();
+        
+        int anioactual = fecha.get(Calendar.YEAR);
+        int mesactual = fecha.get(Calendar.MONTH);
+        int diaactual = fecha.get(Calendar.DAY_OF_MONTH);
+    
+        int aniocalculado = anioactual - iFecha.getAnio();
+        int mescalculado = mesactual - iFecha.getMes();
+        int diacalculado = diaactual - iFecha.getDia();
+        System.out.println("Su tiempo es: "+aniocalculado+" años "+mescalculado+" meses "+diacalculado+" días");
+    }
+    
+    
+    public void verDatos(){
+        System.out.println("Nombre:" +this.nombre);
+        System.out.println("Fecha de Nacimiento: "+fechaN.getAnio()+"/"+fechaN.getMes()+"/"+fechaN.getDia());
+        calculoEdad(fechaN);
+        System.out.println("Fecha de Ingreso: "+fechaI.getAnio()+"/"+fechaI.getMes()+"/"+fechaI.getDia());
+        calculoTiempo(fechaI);
+  
+    }
+    
+//    @Override
+//    public String toString(){
+//        return "Nombre: "+this.nombre + "\n"+"Fecha Nacimiento: "+ this.fechaN +"\n"+ "Fecha Ingreso " +this.fechaI
+//               ;
+//    }
+//    
+
+     
+}
